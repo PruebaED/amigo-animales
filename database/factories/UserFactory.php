e6882cc97b1f93f3c6c620eaf73f9169f46f8e4e
alumno <alumno@vm1>
2021-05-13 19:36:52 +0200

    fix(UserFactory genera los emails sin caracteres especiales)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,50 @@ class UserFactory extends Factory
     $secondSurname = $faker->lastName;
     $uniqueFakeEmail = strtolower($firstSurname . $secondSurname . '@' . $faker->freeEmailDomain);
 
+    /* Quitamos los caracteres especiales de los fake email que vamos generando */
+    //Reemplazamos la A y a
+    $uniqueFakeEmail = str_replace(
+      array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+      array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+      $uniqueFakeEmail
+    );
+ 
+    //Reemplazamos la E y e
+    $uniqueFakeEmail = str_replace(
+      array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+      array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+      $uniqueFakeEmail
+    );
+ 
+    //Reemplazamos la I y i
+    $uniqueFakeEmail = str_replace(
+      array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+      array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+      $uniqueFakeEmail 
+    );
+ 
+    //Reemplazamos la O y o
+    $uniqueFakeEmail = str_replace(
+      array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+      array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+      $uniqueFakeEmail 
+    );
+ 
+    //Reemplazamos la U y u
+      $uniqueFakeEmail = str_replace(
+      array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+      array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+      $uniqueFakeEmail 
+    );
+ 
+    //Reemplazamos la N, n, C y c
+    $uniqueFakeEmail = str_replace(
+      array('Ñ', 'ñ', 'Ç', 'ç'),
+      array('N', 'n', 'C', 'c'),
+      $uniqueFakeEmail
+    );
+    /* ... */
+
     return [
       'name' => $faker->firstName,
       'surnames' => $firstSurname . ' ' . $secondSurname,
